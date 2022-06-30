7593430f475619bbed54bf70d273d69303e464d1
JoshHughes156 <joshhughes2005@yahoo.co.uk>
2022-06-30 11:17:32 +0100

    Reviews page filled out

--- review.js
+++ review.js
@@ -7,8 +7,8 @@ function addReview(name, description) {
 }
 
 function loadReviewsList() {
-    var castnames = ["IGN: 4/5", "Times: 5/5"]; // Hard coded as I cannot figure out how to read from a text file
-    var descriptions = ["Too many stars", "Lightsabers look cool"];
+    var castnames = ["IGN: 4/5", "Times: 5/5", "Guardian: 8/10", "The Sun: 9/10", "The Daily Mail: 6/5", "StarWarsReviews101: 10/10", "IMDB: 8.6"]; // Hard coded as I cannot figure out how to read from a text file
+    var descriptions = ["Too many stars", "Lightsabers look cool", "Timeless classic", "Truely a pleasure to watch", "A start to an amazing trilogy", "R2D2 carried the whole movie, without him it'd be 1/10", "Stupendous"];
     for (let i = 0; i < castnames.length; i++) {
         addReview(castnames[i], descriptions[i]);
     }
